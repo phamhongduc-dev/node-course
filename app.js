8a706d5cdf04cc890fdd75dec8fb7a780c8a1779
pham_duc <duc.pham@beetech.com.vn>
2023-04-23 01:32:02 +0700

    09 - fs.writeFileAsync & fs.appendFileAsync

---
+++ app.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', 'May name is PhamDuc.')
+
+// Challenge: Append a message to notes.txt
+
+// 1. Use appendFileSync to append to the file
+fs.appendFileSync('notes.txt', ' I 22 year old', )
