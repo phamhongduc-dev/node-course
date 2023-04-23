969685b0f0aca97de80f896c6955206db41475bd
pham_duc <duc.pham@beetech.com.vn>
2023-04-23 08:24:41 +0700

    15. Getting Input from Users: process.argv

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const getNotes = require("./notes.js");
 const chalk = require("chalk");
 
-const mes = getNotes();
-console.log(mes);
+const command = process.argv[2];
 
-console.log(chalk.red.inverse.bold("Success...?"));
+console.log(process.argv);
+
+if(command === 'add'){
+    console.log('Adding note!');
+}else if(command === 'remove'){
+    console.log('Removing note!');
+}
