e1801c12f7cd114161a0c17c0f383435c696aeb9
pham_duc <duc.pham@beetech.com.vn>
2024-07-05 01:25:37 +0700

    154. Getting Started with Socket.io

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDiretoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDiretoryPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('New WebSocket connection');
+});
+
+server.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
